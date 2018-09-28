03389e9a783428533f98bcf933eb6c5015d814f0
Rome4 <roman.millem@gmail.com>
2018-09-28 09:01:23 -0400

    Update problem5.cpp

--- ASSIGNMENT2/problem5.cpp
+++ ASSIGNMENT2/problem5.cpp
@@ -1,25 +1,40 @@
+#include <cstddef>
+using namespace std;
 
 template <typename Object>
 
-class stack{
+class stack {
+private:
+    struct node{
+        node() {
+            next = NULL;
+        }
+        node(Object obj) : data(obj) {}                         //Node structure initialization list for data object
+        node(Object obj, node* ptr) : data(obj), next(ptr) {}   //and for both data object and pointer
+        Object data;
+        node* next;
+    };
 
 public:
-    void push(Object x){
-    }
+    stack(){
+        head = NULL;                                    //Stack constructor to initialize head pointer and size
+        SIZE = 0;
+    };
 
-    void pop(){
+    void push(Object obj){                              //Push implementation, create new node for first
+        node* itr = new node(obj,head);                 //added object and make it the head
+        head = itr;
+        ++SIZE;
     }
-
-    void top(){
-    }
-
-    int size(){
-    }
-
-    bool empty(){
+    void pop(){                                         //Pop implementation, assign new node the value of the
+        node* itr = head->next;                         //node head is linked with. Delete head and set head to the
+        delete head;                                    //created node
+        head = itr;
+        --SIZE;
     }
 
 private:
-
+    node * head;
+    int SIZE;
 
 };
